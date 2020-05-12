c99cb73cde67fd9b2b3e40faedfc52cc93cdbeb9
Sid <sidneym@hotmail.nl>
2020-05-12 13:36:08 +0200

    Logic for authorization header
    Created class "SetAuthHeader" to create a Authorization header in the client to make request when integration testing with a hardcoded JWT token for test purposes.

--- Petible-api-testing/integration-test/UserInfoTests.cs
+++ Petible-api-testing/integration-test/UserInfoTests.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Petible_api;
+using Petible_api.Repository;
 
 namespace Petible_api_testing.integration_test
 {
-    class UserInfoTests
+    [TestClass]
+    public class UserInfoTests
     {
+        private readonly HttpClient _client;
+        public UserInfoTests()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            _client = appFactory.CreateClient();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
     }
 }
---
+++ Petible-api-testing/setup-logic/SetAuthHeader.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petible_api_testing.setup_logic
+{
+    public class SetAuthHeader
+    {
+        public async Task<HttpClient> SetAuthorizationHeader(HttpClient client)
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync(client));
+            return client;
+        }
+
+        private async Task<string> GetJwtAsync(HttpClient _client)
+        {
+            var info = new MockLoginInfo
+            {
+                email = "test@petible.nl",
+                password = "test123",
+                returnSecureToken = true
+            };
+            var json = JsonConvert.SerializeObject(info);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var token = await _client.PostAsync("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCUCz3zW6Q21Qf4tuKmCL9vYT6AlygCH1M", content);
+            var tokenresponse = await token.Content.ReadAsStringAsync();
+
+            return tokenresponse;
+        }
+    }
+}
