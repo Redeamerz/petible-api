252de3fae8e232261e10a4cd22b96a8867b1a3e4
daborbor <github@borispeeters.nl>
2020-05-26 12:04:12 +0200

    Fix build error

--- Petible-api/Repository/ReviewRepository.cs
+++ Petible-api/Repository/ReviewRepository.cs
@@ -3,7 +3,7 @@ using Petible_api.Models;
 
 namespace Petible_api.Repository
 {
-    public class ReviewRepository : Repository<Reviews, string>, IUserInfoRepository
+    public class ReviewRepository : Repository<Reviews, string>, IReviewRepository
     {
         public ReviewRepository(IUnitOfWork uow): base(uow)
         {
