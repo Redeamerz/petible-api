1b3afd0e1f74886889c53ee662748fe0beffa054
daborbor <7836056+daborbor@users.noreply.github.com>
2020-06-02 10:43:24 +0200

    Should work now

--- Petible-api/Repository/ReviewsRepository.cs
+++ Petible-api/Repository/ReviewsRepository.cs
@@ -3,9 +3,9 @@ using Petible_api.Models;
 
 namespace Petible_api.Repository
 {
-    public class AnimalShelterRepository : Repository<AnimalShelter, string>, IAnimalShelterRepository
+    public class ReviewRepository : Repository<AnimalShelter, string>, IAnimalShelterRepository
     {
-        public AnimalShelterRepository(IUnitOfWork uow) : base(uow)
+        public ReviewRepository(IUnitOfWork uow) : base(uow)
         {
 
         }
