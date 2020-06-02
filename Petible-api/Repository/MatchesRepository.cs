411a93e14d94a3fe3390589c5ab435f7805ae6bd
daborbor <7836056+daborbor@users.noreply.github.com>
2020-06-02 10:40:12 +0200

    Paar files vergeten

---
+++ Petible-api/Repository/MatchesRepository.cs
@@ -0,0 +1,13 @@
+﻿using Petible_api.Interfaces;
+using Petible_api.Models;
+
+namespace Petible_api.Repository
+{
+    public class MatchesRepository : Repository<Matches, string>, IMatchesRepository
+    {
+        public MatchesRepository(IUnitOfWork uow) : base(uow)
+        {
+
+        }
+    }
+}
