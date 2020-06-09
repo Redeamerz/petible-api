9f48492f165b7b1bbc532048d3aa38ffc07053df
Sid <sidneym@hotmail.nl>
2020-06-09 14:07:14 +0200

    Added pet interface and repo to startup

--- Petible-api/Startup.cs
+++ Petible-api/Startup.cs
@@ -76,6 +76,7 @@ namespace Petible_api
             services.AddScoped<IUnitOfWork, NHUnitOfWork>();
             services.AddTransient<IUserInfoRepository, UserInfoRepository>();
             services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IPetRepository, PetRepository>();
             services.AddTransient<IAnimalShelterRepository, AnimalShelterRepository>();
             services.AddTransient<IReviewRepository, ReviewRepository>();
             services.AddControllersWithViews();
