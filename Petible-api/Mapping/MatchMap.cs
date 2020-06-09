7109b6245e93bd86f1d477e8da882f5b9f81469b
Sid <sidneym@hotmail.nl>
2020-06-09 15:28:06 +0200

    Changed variables in map and model to match table

--- Petible-api/Mapping/MatchMap.cs
+++ Petible-api/Mapping/MatchMap.cs
@@ -14,7 +14,9 @@ namespace Petible_api.Mapping
             Table("matches");
             Id(x => x.id).Column("id");
             Map(x => x.pet_id).Column("pet_id");
-            Map(x => x.unmatched).Column("unmatched");
+            Map(x => x.user_id).Column("user_id");
+            Map(x => x.status).Column("status");
+            Map(x => x.shelterEmail).Column("shelteremail");
         }
     }
 }
