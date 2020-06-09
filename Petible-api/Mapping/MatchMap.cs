63769663c15ed2981b5ce422ec6eed896df5fef2
Sid <sidneym@hotmail.nl>
2020-06-09 15:15:31 +0200

    Model en Mapping matched with database table

--- Petible-api/Mapping/MatchMap.cs
+++ Petible-api/Mapping/MatchMap.cs
@@ -7,14 +7,20 @@ using System.Threading.Tasks;
 
 namespace Petible_api.Mapping
 {
-    public class MatchesMap : ClassMap<Match>
+    public class MatchMap : ClassMap<Match>
     {
-        public MatchesMap()
+        public MatchMap()
         {
             Table("matches");
             Id(x => x.id).Column("id");
             Map(x => x.pet_id).Column("pet_id");
-            Map(x => x.unmatched).Column("unmatched");
+            Map(x => x.timeFree).Column("timefree");
+            Map(x => x.name).Column("name");
+            Map(x => x.dateOfBirth).Column("dateofbirth");
+            Map(x => x.gender).Column("gender");
+            Map(x => x.description).Column("description");
+            Map(x => x.inAnimalShelterSince).Column("inanimalsheltersince");
+            Map(x => x.fees).Column("fees");
         }
     }
 }
--- Petible-api/Models/Match.cs
+++ Petible-api/Models/Match.cs
@@ -5,10 +5,16 @@ using System.Threading.Tasks;
 
 namespace Petible_api.Models
 {
-    public class Matches
+    public class Match
     {
         public virtual string id { get; set; }
-        public virtual int pet_id { get; set; }
-        public virtual bool unmatched { get; set; }
+        public virtual string pet_id { get; set; }
+        public virtual int timeFree { get; set; }
+        public virtual string name { get; set; }
+        public virtual DateTime dateOfBirth { get; set; }
+        public virtual bool gender { get; set; }
+        public virtual string description { get; set; }
+        public virtual DateTime inAnimalShelterSince { get; set; }
+        public virtual int fees { get; set; }
     }
 }
