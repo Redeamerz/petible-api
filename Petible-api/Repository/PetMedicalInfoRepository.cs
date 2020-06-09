bb0566ac8b7175d9d6fffdb5837119ebd2079e4c
Sid <sidneym@hotmail.nl>
2020-06-09 15:45:26 +0200

    Added repository

---
+++ Petible-api/Repository/PetMedicalInfoRepository.cs
@@ -0,0 +1,17 @@
+﻿using Petible_api.Interfaces;
+using Petible_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Petible_api.Repository
+{
+    public class PetMedicalInfoRepository : Repository<PetMedicalInfo, string>, IPetMedicalInfoRepository
+    {
+        public PetMedicalInfoRepository(IUnitOfWork uow) : base(uow)
+        {
+
+        }
+    }
+}
