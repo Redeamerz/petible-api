ab89355f8987f1ed5270e235789426b79f597600
Redeamer <wesleytuuh@gmail.com>
2020-06-09 13:47:50 +0200

    Create match status enum

---
+++ Petible-api/enums/matchStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Petible_api.enums
+{
+	public enum matchStatus
+	{
+		DENIED = 1,
+		AWAITING,
+		ACCEPTED
+	}
+}
