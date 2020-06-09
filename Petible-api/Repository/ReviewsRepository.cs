cd793c033dfb44326e98cd7dba903b8fd479ed91
Sid <sidneym@hotmail.nl>
2020-06-09 09:37:16 +0200

    Add repository and repository interface

---
+++ Petible-api/Repository/ReviewsRepository.cs
@@ -0,0 +1,17 @@
+﻿using Petible_api.Interfaces;
+using Petible_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Petible_api.Repository
+{
+    public class ReviewsController : Repository<Reviews, string>, IReviewsRepository
+    {
+        public ReviewsRepository(IUnitOfWork uow) : base(uow)
+        {
+
+        }
+    }
+}
